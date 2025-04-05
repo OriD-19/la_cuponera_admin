79bfc585615e3127c07ca1b63314535b4193ecdc
FernandaRubio74 <20235795@esen.edu.sv>
2025-04-04 18:32:45 -0600

    route

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
 
     return (
         <nav className="peer-checked:flex hidden absolute z-20 top-full left-0 w-full bg-white flex-col items-center shadow-lg md:relative md:top-0 md:flex md:flex-row md:w-auto md:space-x-6 md:bg-transparent md:shadow-none">
-            <NavLink to="/home" className={({ isActive }) => `inline-flex items-center text-black ${isActive ? "text-primary font-bold text-2xl" : ""}`}>
+            <NavLink to="/offers" className={({ isActive }) => `inline-flex items-center text-black ${isActive ? "text-primary font-bold text-2xl" : ""}`}>
                 Ofertas
             </NavLink>
 
@@ -22,6 +22,10 @@ const Navigation = () => {
 
             <NavLink to="/category" className={({ isActive }) => `inline-flex items-center text-black ${isActive ? "text-primary font-bold text-2xl" : ""}`}>
                 Categorías
+            </NavLink>
+
+            <NavLink to="/enterprise" className={({ isActive }) => `inline-flex items-center text-black ${isActive ? "text-primary font-bold text-2xl" : ""}`}>
+                Empresas
             </NavLink>
 
 
